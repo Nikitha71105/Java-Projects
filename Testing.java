e29182b6e3ee2e5e9bed114a468e6967d3be004d
Kuncha Nikitha <kunchanikitha1451.sse@saveetha.com>
2026-04-08 08:54:50 +0530

    kept only E38.java

--- Testing.java
+++
@@ -1,9 +0,0 @@
-class Testing extends Thread{
-    public void run(){
-        System.out.println("Running...");
-    }
-        public static void main(String [] args){
-            Testing t = new Testing();
-            t.start();
-        }
-    }
--- p1.java
+++
@@ -1,8 +0,0 @@
-public class p1 {
-    public static void main(String[] args) {
-     String x = new String ("Java");
-     String y = "Java";
-        System.out.println(x.equals(y) + " " + (x == y));   
-    }
-    
-}
